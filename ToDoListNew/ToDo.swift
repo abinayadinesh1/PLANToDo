15c04e73ee2fae3357369224168c61a725eb9626
Abinaya Dinesh <abinayadinesh1@gmail.com>
2020-06-09 09:27:48 -0400

    Iteration 0: Complete

---
+++ ToDoListNew/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoListNew
+//
+//  Created by Abinaya Dinesh on 6/8/20.
+//  Copyright © 2020 Abinaya Dinesh. All rights reserved.
+//
+
+import UIKit
+
+class ToDo{
+    var name = ""
+    var important = Bool()
+}
+
