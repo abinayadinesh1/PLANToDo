75ff6e08d329a0a439dcf4acce58da9c13b7512f
Angela Huynh <angelathuynh5@gmail.com>
2020-06-11 22:35:36 -0400

    resources vc

---
+++ ToDoListNew/resourcesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  resourcesViewController.swift
+//  ToDoListNew
+//
+//  Created by Angela Huynh on 6/11/20.
+//  Copyright © 2020 Abinaya Dinesh. All rights reserved.
+//
+
+import UIKit
+
+class resourcesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
